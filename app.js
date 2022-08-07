9bded8a581ced1a206bc427217910c2bd0779b4c
joel <joelxcabana@gmail.com>
2022-08-07 19:15:37 -0400

    creacion de las rutas e importacion dinamicamente, agrege el script para nodemon

--- app.js
+++ app.js
@@ -5,9 +5,13 @@ const cors = require('cors')
 const app = express()
 
 const dataBaseConnect = require('./config/mongo')
-
 app.use(cors())
+
 const port = process.env.PORT || 3000
+
+// rutas
+app.use('/api/',require('./routes'))
+
 app.listen(port,()=> {
     console.log(`listen on por ${port}`)
 })
